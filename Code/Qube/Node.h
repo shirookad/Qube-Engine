5153cbc2049332574311f0d2bfdc933aadc3ec46
Johannes Roth <johannes.roth@aol.com>
2015-05-10 11:00:04 +0200

    Added setName method to Node class

--- Code/Qube/Node.h
+++ Code/Qube/Node.h
@@ -15,6 +15,9 @@ namespace qube {
 	public:
 		QUBE_API Node(std::string name);
 
+		template <class String, class = std::enable_if_t<!std::is_same<std::decay_t<String>, std::string>::value>>
+		inline void setName(String&& name) noexcept(std::is_nothrow_assignable<std::string&, String>::value) { m_name = std::forward<String>(name); }
+
 		inline const std::string& name() const { return m_name; }
 	};
 }
