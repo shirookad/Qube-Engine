20386f04b7423c94e4393fbdb0d287cfe2922cb2
Johannes Roth <johannes.roth@aol.com>
2015-05-10 17:30:22 +0200

    Added Node hierarchy system

--- Code/Qube/Node.cpp
+++ Code/Qube/Node.cpp
@@ -1,6 +1,32 @@
 #include "stdafx.h"
 #include "Node.h"
 
-qube::Node::Node(std::string name)
-	: m_name(std::move(name)) {
+namespace qube {
+	Node::Node(std::string name)
+		: m_name(std::move(name)) {
+	}
+
+	Node::Node(std::string name, Node *parent)
+		: m_name(std::move(name))
+		, m_parent(parent) {
+	}
+
+	std::shared_ptr<Node> Node::addChild(const std::string& name) {
+		auto child = std::make_shared<Node>(name, this);
+		m_children.push_back(child);
+		return child;
+	}
+
+	std::shared_ptr<Node> Node::addChild(std::string&& name) {
+		auto child = std::make_shared<Node>(name, this);
+		m_children.push_back(child);
+		return child;
+	}
+
+	void Node::iterateThroughChildHierarchy(std::function<void(const std::shared_ptr<Node>&)> func) const {
+		for (const auto& child : m_children) {
+			func(child);
+			child->iterateThroughChildHierarchy(func);
+		}
+	}
 }
--- Code/Qube/Node.h
+++ Code/Qube/Node.h
@@ -6,19 +6,32 @@
 #include "ProjectDefines.h"
 
 #include <string>
+#include <vector>
+#include <memory>
+#include <functional>
 
 namespace qube {
 	class Node {
 	private:
 		std::string m_name;
+		std::vector<std::shared_ptr<Node>> m_children;
+		Node *m_parent{ nullptr };
 
 	public:
 		QUBE_API Node(std::string name);
+		QUBE_API Node(std::string name, Node *parent);
+
+		QUBE_API std::shared_ptr<Node> qube::Node::addChild(const std::string& name);
+		QUBE_API std::shared_ptr<Node> qube::Node::addChild(std::string&& name);
+
+		QUBE_API void iterateThroughChildHierarchy(std::function<void(const std::shared_ptr<Node>&)> func) const;
 
 		template <class String, class = std::enable_if_t<!std::is_same<std::decay_t<String>, std::string>::value>>
 		inline void setName(String&& name) noexcept(std::is_nothrow_assignable<std::string&, String>::value) { m_name = std::forward<String>(name); }
 
-		inline const std::string& name() const { return m_name; }
+		inline const auto& name() const { return m_name; }
+		inline const auto& children() const { return m_children; }
+		inline Node *parent() const { return m_parent; }
 	};
 }
 
