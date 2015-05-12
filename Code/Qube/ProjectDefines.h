206d0551bf15d491e7b0318ce65c5bfe59ec720d
Johannes Roth <johannes.roth@aol.com>
2015-05-12 16:54:09 +0200

    Added sized number definitions to Qube/ProjectDefines.h

--- Code/Qube/ProjectDefines.h
+++ Code/Qube/ProjectDefines.h
@@ -18,4 +18,23 @@
 #	endif
 #endif
 
+#include <cstdint>
+namespace qube {
+	using int8 = std::int8_t;
+	using int16 = std::int16_t;
+	using int32 = std::int32_t;
+	using int64 = std::int64_t;
+
+	using uint8 = std::uint8_t;
+	using uint16 = std::uint16_t;
+	using uint32 = std::uint32_t;
+	using uint64 = std::uint64_t;
+
+	using float32 = float;
+	using float64 = double;
+
+	// 32bit or 64bit
+	using sfloat = float32;
+}
+
 #endif
